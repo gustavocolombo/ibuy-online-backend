b7d58d8c5a322bd29e1776798f80345a26086496
Gustavo Colombo <gusttavocolombo@gmail.com>
2021-02-20 15:57:35 -0300

    changing relations of models

--- src/database/migrations/1613684079353-CreateFKSellerProduct.ts
+++ src/database/migrations/1613684079353-CreateFKSellerProduct.ts
@@ -10,13 +10,14 @@ export class CreateFKSellerProduct1613684079353 implements MigrationInterface {
       new TableColumn({
         name: 'seller_id',
         type: 'uuid',
+        default: 'uuid_generate_v4()',
       }),
     );
 
     await queryRunner.createForeignKey(
       'product',
       new TableForeignKey({
-        name: 'SellerForSale',
+        name: 'SellerOfProduct',
         columnNames: ['seller_id'],
         referencedColumnNames: ['id'],
         referencedTableName: 'seller',
@@ -27,7 +28,7 @@ export class CreateFKSellerProduct1613684079353 implements MigrationInterface {
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.dropForeignKey('sale', 'ProductToSale');
+    await queryRunner.dropForeignKey('sale', 'SellerOfProduct');
     await queryRunner.dropColumn('product', 'seller_id');
   }
 }
